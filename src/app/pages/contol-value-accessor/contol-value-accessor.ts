a2be9ae6df61b5aa5253c2f6ab49f743939c9a26
Willian Galvez Fernández <willian.galvez91@gmail.com>
2026-03-02 22:33:03 +0100

    feat: new styles files for pages

--- src/app/pages/contol-value-accessor/contol-value-accessor.ts
+++ src/app/pages/contol-value-accessor/contol-value-accessor.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { JsonPipe } from '@angular/common';
 import { ChildInput } from './child-input/child-input';
 
 @Component({
   selector: 'app-contol-value-accessor',
-  imports: [ReactiveFormsModule, JsonPipe, ChildInput],
+  imports: [ReactiveFormsModule, ChildInput],
   templateUrl: './contol-value-accessor.html',
   styleUrl: './contol-value-accessor.scss',
 })
